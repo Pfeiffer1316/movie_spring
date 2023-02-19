13bbde28704cc1c772789c12fa4ca94da27a7016
Pfeiffer1316 <sjinwo0@naver.com>
2023-02-19 16:57:43 +0900

    commit

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,6 +3,7 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -34,14 +35,20 @@ public class DemoApplication {
 		}
 
 		public String requestParam(
-		@RequestParam(required = false, value = "id") String id, 
-		@RequestParam(required = false, value = "pw") String pw, 
-		@RequestParam(required = false, value = "nickname") String nickname,
-		@RequestParam(required = false, value = "email") String email,
-		@RequestParam(required = false, value = "recommender") String recommender){
+				@RequestParam(required = false, value = "id") String id,
+				@RequestParam(required = false, value = "pw") String pw,
+				@RequestParam(required = false, value = "nickname") String nickname,
+				@RequestParam(required = false, value = "email") String email,
+				@RequestParam(required = false, value = "recommender") String recommender, Model model) {
 
-		
-			return "signUp";
+			model.addAttribute("id", id);
+			model.addAttribute("pw", pw);
+			model.addAttribute("nickname", nickname);
+			model.addAttribute("email", email);
+			model.addAttribute("recommender", recommender);
+
+			return "redirect:/signUpTest.html?id=" + id + "&pw=" + pw + "&nickname=" + nickname + "&email=" + email
+					+ "&recommender=" + recommender;
 		}
 
 		@GetMapping(value = "/signUpTest.html")
