7a328318bb9de038117955505770cef7f8e07950
Pfeiffer1316 <sjinwo0@naver.com>
2023-03-01 00:58:10 +0900

    MySQL 연결(DBeaver), Entity 오류

---
+++ src/main/java/com/example/demo/user.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.hibernate.annotations.Entity;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity(name="test")
+public class user {
+    
+}
